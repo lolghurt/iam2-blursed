af0cfdbf2fdca0107f39887095c12765ca35a3e0
lolghurt <lolghurt@hotmail.com>
2020-07-16 01:35:41 +1000

    goddamnit gitignore

--- src/data/scripts/aBadIdea2_ModPlugin.java
+++ src/data/scripts/aBadIdea2_ModPlugin.java
@@ -7,24 +7,17 @@ import exerelin.campaign.SectorManager;
 
 import static com.fs.starfarer.api.Global.getSettings;
 
-public class aBadIdea2_ModPlugin extends BaseModPlugin {/*
-    @Override
-    public void onApplicationLoad() throws Exception {
-        boolean hasPart2 = getSettings().getModManager().isModEnabled("aBadIdea2");
-        boolean hasPart3 = getSettings().getModManager().isModEnabled("aBadIdea3");
-        if (!hasPart2) {
-            throw new RuntimeException("The MEME FACTION requires MEME SHIPS (part 2)");
-        }
-        if (!hasPart3) {
-            throw new RuntimeException("The MEME FACTION requires MEME SHIPS (part 3)");
-        }
-    }*/
+public class aBadIdea2_ModPlugin extends BaseModPlugin {
     @Override
     public void onNewGame() {
+        boolean sanity1 = getSettings().getModManager().isModEnabled("new_galactic_order");
+        boolean sanity2 = getSettings().getModManager().isModEnabled("aria");
         //Nex compatibility setting, if there is no nex or corvus mode(Nex), just generate the system
         boolean haveNexerelin = Global.getSettings().getModManager().isModEnabled("nexerelin");
         if (!haveNexerelin || SectorManager.getCorvusMode()) {
-            new aBadIdea2_WorldGen().generate(Global.getSector());
+            if(!sanity1 && !sanity2) {
+                new aBadIdea2_WorldGen().generate(Global.getSector());
+            }
         }
     }
 }
